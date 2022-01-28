63e4218ab67eea7d015a68104b51329cce1181a3
oktubr3 <mauroh@gmail.com>
2022-01-28 10:16:39 -0300

    Agregados span de abcedario con ids

--- scripts/app.js
+++ scripts/app.js
@@ -1,7 +1,9 @@
 const letters = document.querySelector("#letters"); // letras en el tablero
+const alph = document.getElementById('alphabet'); // letras del alfabeto
 
 const words = [ 'manzana', 'banana', 'pera', 'perro', 'gato', 'rinoceronte', 'bicicleta', 'escopeta', 'canario' ]
 console.log(Math.floor(Math.random() * words.length));
+const alphabet = 'abcdefghijklmnñopqrstuvwxyz'.split('');
 
 const wordSelected = words[Math.floor(Math.random() * words.length)]; // Seleccionamos una palabra al azar multiplicando el numero aleatorio por la longitud del array
 console.log(wordSelected);
@@ -18,6 +20,14 @@ arrWord.forEach((letter) => {
     }
 );
 letters.innerHTML = guiones.join(' ');
+
+alphabet.forEach((letter) => {
+    const span = document.createElement('span');
+    span.innerHTML = letter.toUpperCase() + ' ';  // Agregamos un espacio despues de cada letra
+    alph.appendChild(span).classList.add('hover:text-blue-700', 'focus:ring', 'text-center', 'cursor-pointer');
+    alph.appendChild(span).id = letter;
+});
+
 
 
 
