a05a1be21d3671e7031a296492e0a4eac07bf7d2
oktubr3 <mauroh@gmail.com>
2022-01-29 22:19:04 -0300

    Agregué images y swith

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,13 @@
-// Conectores:
 const letters = document.querySelector("#letters"); // letras en el tablero
 const alph = document.getElementById('alphabet'); // letras del alfabeto
 let lives = document.getElementById('nro-vidas'); // vidas
+const image = document.getElementById('image'); // imagen
 
 
-const words = [ 'manzana', 'banana', 'pera', 'perro', 'gato', 'rinoceronte', 'bicicleta', 'escopeta', 'canario' ];
+const words = ['manzana', 'banana', 'pera', 'perro', 'gato', 'rinoceronte', 'bicicleta', 'escopeta', 'canario'];
 console.log(Math.floor(Math.random() * words.length));
 const alphabet = 'abcdefghijklmnñopqrstuvwxyz'.split('');
-let vidas = 10;
+let vidas = 8;
 lives.innerHTML = vidas;
 
 const wordSelected = words[Math.floor(Math.random() * words.length)]; // Seleccionamos una palabra al azar multiplicando el numero aleatorio por la longitud del array
@@ -22,15 +22,19 @@ let guionesArr = [];
 
 arrWord.forEach((letter) => {
     guionesArr.push('_');
-    }
+}
 );
 letters.innerHTML = guionesArr.join(' ');
 
+
+
+// Dibujo el alfabeto en el tablero
 alphabet.forEach((letter) => {
     const span = document.createElement('span');   // Creamos un elemento span
     span.innerHTML = letter.toUpperCase() + ' ';   // Agregamos la letra mas un espacio
     alph.appendChild(span).classList.add('hover:text-blue-800', 'cursor-pointer');
     alph.appendChild(span).id = letter;
+    // Agregamos el evento click a cada letra del alfabeto
     span.addEventListener('click', (e) => {
         let letterClicked = e.target.id;
         let letterIndex = arrWord.indexOf(letterClicked);
@@ -47,17 +51,55 @@ alphabet.forEach((letter) => {
             setTimeout(() => {
                 alph.removeChild(span).id;
             }
-            , 1500);
+                , 1500);
         }
+        switch (vidas) {
+            case 8:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc7v.png"></img>';
+                break;
+            case 7:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc7v.png"></img>';
+                break;
+            case 6:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc6v.png"></img>';
+                break;
+            case 5:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc5v.png"></img>';
+                break;
+            case 4:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc4v.png"></img>';
+                break;
+            case 3:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc3v.png"></img>';
+                break;
+            case 2:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc2v.png"></img>';
+                break;
+            case 1:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc1v.png"></img>';
+                break;
+            case 0:
+                image.innerHTML = '<img class="mx-auto h-80 w-80" src="../assets/images/ahorc0vxx.png"></img>';
+                break;
+        }
+        // Si se quedó sin vidas pierde
         if (vidas === 0) {
-            alert('Perdiste, seguí intentando, la palabra era: ' + wordSelected);
+            alph.innerHTML = 'Perdiste, la palabra era: ' + wordSelected;
+            alph.classList.add('text-red-600');
         }
         if (guionesArr.join('') === arrWord.join('')) {
             alph.innerHTML = '';
             alph.innerHTML = 'Ganaste!!!';
+            alph.classList.add('text-green-600');
         }
+        
     });
 });
+
+
+
+
+
 
 
 
